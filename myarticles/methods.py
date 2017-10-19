3066928d15ffa94326b1d3e9c20ea87a169e37ed
hdknr <gmail@hdknr.com>
2017-10-19 22:30:04 +0900

    rendering contents for publishing

--- myarticles/methods.py
+++ myarticles/methods.py
@@ -1,8 +1,49 @@
 # coding: utf-8
 from django.core.urlresolvers import reverse
+from django.template.loader import render_to_string
 from mywords.models import Word
 from corekit.methods import CoreModel
 from . import signals
+
+
+class Graph(object):
+
+    def __init__(self, element, parent):
+        self.parent = parent
+        self.element = element.instance
+        self.children = []
+
+    @property
+    def container_level(self):
+        return self.element.container_level
+
+    def add_child(self, element):
+        new = Graph(element, self)
+        self.children.append(new)
+        return self
+
+    def add_holder(self, element):
+        new = Graph(element, self)
+        self.children.append(new)
+        return new
+
+    def add(self, element):
+        if not element.is_container:
+            return self.add_child(element)
+
+        current = self
+        if current.parent:
+            if element.container_level == current.container_level:
+                current = current.parent
+            elif not current.parent.parent:
+                current = current.parent
+            elif element.container_level < current.container_level:
+                current = current.parent.parent
+        current = current.add_holder(element)
+        return current
+
+    def render_content(self):
+        return self.element.render_content(graph=self)
 
 
 class Article(CoreModel):
@@ -16,8 +57,49 @@ class Article(CoreModel):
                     word.link_set.get_or_create(
                         content_type=self.contenttype(), object_id=self.id)
 
+    @property
+    def graph(self):
+        root = Graph(self, None)
+        current = root
+
+        for e in self.element_set.all():
+            current = current.add(e)
+
+        return root
+
+    def render_content(self):
+        return render_to_string(
+            'articles/article/content.html',
+            context=dict(graph=self.graph))
+
 
 class Element(CoreModel):
 
     def get_absolute_url(self):
         return reverse('myarticles_element_detail', kwargs={'id': self.id})
+
+    @property
+    def element_name(self):
+        '''element name'''
+        return self.instance._meta.model_name
+
+    @property
+    def element_fullname(self):
+        '''app_label.model_name '''
+        return ".".join(self.contenttype().natural_key())
+
+    @property
+    def is_container(self):
+        return self.element_name in ['section', 'subsection']
+
+    @property
+    def container_level(self):
+        try:
+            return ['section', 'subsection'].index(self.element_name) + 2
+        except:
+            return ''
+
+    def render_content(self, **kwargs):
+        return render_to_string(
+            'articles/element/{}/content.html'.format(self.element_fullname),
+            context=kwargs)
