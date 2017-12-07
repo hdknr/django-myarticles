9105b2680dc6babb2043cbd0769b639c08418390
hdknr <gmail@hdknr.com>
2017-12-07 15:20:17 +0900

    Text editor (TODO: textarea styling)

--- myarticles/templates/articles/element/vue/myarticles_text.js
+++ myarticles/templates/articles/element/vue/myarticles_text.js
@@ -1,13 +1,14 @@
 var myArticlesText = Vue.extend({
+  props: ['value', ],
   template: '#myarticles_text_template',
-  props: ['value', ],   
-  data: function(){
-    return {
-    };
-  },
-  created(){
-  },
+  mixins: [myArticleElement],
+  components:{ 'mymedia-text': TextComponent, },
   methods: {
+    update(texts){
+        this.value.texts = texts;
+        this.send(this.value).then((res) =>{
+            this.$emit('input', this.value);
+        });
+    }
   }
 });
-
