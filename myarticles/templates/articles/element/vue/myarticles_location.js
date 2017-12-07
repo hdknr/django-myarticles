c73f2f26197005b01c6cad379926135b4578fd27
hdknr <gmail@hdknr.com>
2017-12-07 15:55:59 +0900

    Location editor

--- myarticles/templates/articles/element/vue/myarticles_location.js
+++ myarticles/templates/articles/element/vue/myarticles_location.js
@@ -1,13 +1,21 @@
-var myArticlesLocation= Vue.extend({
+var myArticlesLocation = Vue.extend({
+  props: ['value', ],
   template: '#myarticles_location_template',
-  props: ['value', ],   
-  data: function(){
-    return {
-    };
-  },
-  created(){
+  mixins: [myArticleElement],
+  components:{ 'mymedia-text': TextComponent, },
+  computed:{
+      location_url(){
+          res = "//maps.google.co.jp/maps?q=" + encodeURI(this.value.address) + "&output=embed&t=m&z=16&iwloc=";
+          console.log("location....", res);
+          return res;
+      }
   },
   methods: {
+    update(){
+        this.send(this.value).then((res) =>{
+            this.$forceUpdate();
+            this.$emit('input', this.value);
+        });
+    }
   }
 });
-
