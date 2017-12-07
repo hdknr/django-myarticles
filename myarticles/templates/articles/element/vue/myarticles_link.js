be8a909076e3f8fc0f49312e48cb643bb29d7d1d
hdknr <gmail@hdknr.com>
2017-12-07 21:29:23 +0900

    Link editor

--- myarticles/templates/articles/element/vue/myarticles_link.js
+++ myarticles/templates/articles/element/vue/myarticles_link.js
@@ -1,13 +1,14 @@
 var myArticlesLink = Vue.extend({
+  props: ['value', ],
   template: '#myarticles_link_template',
-  props: ['value', ],   
-  data: function(){
-    return {
-    };
-  },
-  created(){
-  },
+  mixins: [myArticleElement],
+  components:{ 'mymedia-text': TextComponent, },
   methods: {
+    update(){
+        this.send(this.value).then((res) =>{
+            Vue.set(this, 'value', res.data);
+            this.$emit('input', this.value);
+        });
+    }
   }
 });
-
